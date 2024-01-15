afaf3f1a625ad983362494a5bc61aeb95c023f22
CamiloDP96 <cadp.96@outlook.com>
2024-01-15 11:18:23 -0500

    project reservation system creation complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { ReservationListComponent } from './reservation-list/reservation-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReservationComponent,
+    LogInComponent,
+    ReservationListComponent
   ],
   imports: [
     BrowserModule
